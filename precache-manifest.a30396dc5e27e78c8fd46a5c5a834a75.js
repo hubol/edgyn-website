302e097e70b87450ac20988878434137d6d4568b
Hubol Persson-Gordon <hubol.gordon@gmail.com>
2019-09-22 20:47:37 -0500

    Updates

---
+++ precache-manifest.a30396dc5e27e78c8fd46a5c5a834a75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c508d966c703bbb9561f0fdd22651b6f",
+    "url": "/edgyn-website/index.html"
+  },
+  {
+    "revision": "2f29d3334c9c27f0e074",
+    "url": "/edgyn-website/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "8dd15bb052cd9d3bb96d",
+    "url": "/edgyn-website/static/js/2.a78e6e57.chunk.js"
+  },
+  {
+    "revision": "2f29d3334c9c27f0e074",
+    "url": "/edgyn-website/static/js/main.68c6eed4.chunk.js"
+  },
+  {
+    "revision": "88808a27d37cf4fbaebf",
+    "url": "/edgyn-website/static/js/runtime-main.e602f0dd.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/edgyn-website/static/media/logo.25bf045c.svg"
+  }
+]);
